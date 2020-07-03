d9a1823fe791f33c1c7680f9228e1b085683ee5c
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-07-03 23:08:43 +0300

    Init summary page

--- frontend/src/pages/excercise/ExerciseSummary.jsx
+++ frontend/src/pages/excercise/ExerciseSummary.jsx
@@ -1,3 +1,57 @@
 import React from "react";
+import { getActivityName } from "../../utils/activityInfo";
+import { unixtimeToString } from "../../utils/timeConverter";
 
-export const ExerciseSummary = () => <h1>Exercise summary</h1>;
+export const ExerciseSummary = ({
+  exerciseList,
+  setFilterStartDate,
+  setFilterEndDate,
+}) => {
+  const onChangeFilterStartDate = ({ target: { value } }) =>
+    setFilterStartDate(value !== "" ? new Date(value).getTime() : null);
+  const onChangeFilterEndDate = ({ target: { value } }) =>
+    setFilterEndDate(value !== "" ? new Date(value).getTime() : null);
+
+  return (
+    <div>
+      <h1>Exercise summary</h1>
+      <div>
+        <h3>Filters</h3>
+        <label>
+          Start date:
+          <input type="date" onChange={onChangeFilterStartDate} />
+        </label>
+        <br />
+        <label>
+          End date:
+          <input type="date" onChange={onChangeFilterEndDate} />
+        </label>
+      </div>
+      <ul>
+        {exerciseList.map((exercise) => (
+          <Exercise {...exercise} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Exercise = ({
+  activityId,
+  startTime,
+  totalTime,
+  totalDistance,
+  totalAscent,
+  totalDescent,
+  maxSpeed,
+  hrmax,
+  hravg,
+  avgSpeed,
+  avgCadence,
+  feeling,
+}) => (
+  <li>
+    {getActivityName(activityId)} {unixtimeToString(startTime)}, time:{" "}
+    {totalTime}, distance: {totalDistance}m, max speed: {maxSpeed}
+  </li>
+);
