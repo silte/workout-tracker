b60597815fb93498134022a7986dd7b69daca90d
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-07-04 23:36:30 +0300

    Add types for activity info

---
+++ frontend/src/types/activityInfo.d.ts
@@ -0,0 +1,9 @@
+interface IActivityInfo {
+  Key: string;
+  STId: number;
+  STKey: string;
+  MCId: number;
+  Name: string;
+  PhraseID: string;
+  Icons?: string[];
+}
--- frontend/src/utils/activityInfo.ts
+++ frontend/src/utils/activityInfo.ts
@@ -1,4 +1,14 @@
-import activityInfoData from '../constants/activityTypes.json';
+import activityInfoData from "../constants/activityTypes.json";
 
-export const getActivityName = (activityId: number) =>
-  (activityInfoData as any).find((activityInfo: any) => activityInfo.STId === activityId).Name;
+export const getActivityName = (activityId: number) => {
+  const activityInfo = getActivity(activityId);
+
+  return typeof activityInfo !== "undefined"
+    ? activityInfo.Name
+    : "unknown activity";
+};
+
+const getActivity = (activityId: number) =>
+  (activityInfoData as IActivityInfo[]).find(
+    (activityInfo) => activityInfo.STId === activityId
+  );
