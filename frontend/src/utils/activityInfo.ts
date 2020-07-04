5efe8faa3e47c27cec219c250c101e1dfc293c42
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-07-04 23:25:49 +0300

    fix frontend eslint warnings

--- frontend/src/utils/activityInfo.ts
+++ frontend/src/utils/activityInfo.ts
@@ -1,4 +1,4 @@
 import activityInfoData from '../constants/activityTypes.json';
 
 export const getActivityName = (activityId: number) =>
-  (<any>activityInfoData).find((activityInfo: any) => activityInfo.STId === activityId).Name;
+  (activityInfoData as any).find((activityInfo: any) => activityInfo.STId === activityId).Name;
