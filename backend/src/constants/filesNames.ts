19b9f6907a7b83c3195ccc6ab21afb0c4db21628
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-07-05 21:45:41 +0300

    Add multisport summary data to part of workoutlist api response

--- backend/src/constants/filesNames.ts
+++ backend/src/constants/filesNames.ts
@@ -1,2 +1,5 @@
 export const DATA_DIR: string = "./data";
 export const WORKOUT_LIST_FILENAME: string = `${DATA_DIR}/workout-list.json`;
+
+export const getWorkoutRawDataFilename = (workoutId: string) =>
+  `${DATA_DIR}/${workoutId}.json`;
--- backend/src/controllers/workout/getWorkoutList.ts
+++ backend/src/controllers/workout/getWorkoutList.ts
@@ -1,12 +1,18 @@
 import { readJson } from "../../utils/jsonHelper";
-import { WORKOUT_LIST_FILENAME } from "../../constants/filesNames";
+import {
+  WORKOUT_LIST_FILENAME,
+  getWorkoutRawDataFilename,
+} from "../../constants/filesNames";
+import { getMultisportExtension } from "./getWorkoutExtension";
+
+const MULTISPORT_ACTIVITY_ID = 68;
 
 export const getWorkoutList = () => {
   const allWorkoutListData: IWorkoutList = readJson(WORKOUT_LIST_FILENAME);
-  return allWorkoutListData.payload.map(filterWorkoutListItemContent);
+  return allWorkoutListData.payload.map(parseWorkoutSummarData);
 };
 
-const filterWorkoutListItemContent = ({
+const parseWorkoutSummarData = ({
   extensions = [],
   hrdata = <IHrdata>{},
   activityId,
@@ -17,6 +23,7 @@ const filterWorkoutListItemContent = ({
   totalAscent,
   totalDescent,
   maxSpeed,
+  energyConsumption,
 }: IWorkoutListItem): IWorkoutSummaryData => {
   const summaryExtension = extensions.find(
     (extension) => extension.type === "SummaryExtension"
@@ -25,6 +32,10 @@ const filterWorkoutListItemContent = ({
   const { hrmax, avg: hravg } = hrdata;
   const { avgSpeed, avgCadence, feeling } =
     typeof summaryExtension !== "undefined" ? summaryExtension : <IExtension>{};
+  const multisportSummary =
+    activityId === MULTISPORT_ACTIVITY_ID
+      ? getWorkoutMultisportSummaryData(workoutKey)
+      : <IWorkoutMultisportSummaryData[]>[];
 
   return {
     activityId,
@@ -40,5 +51,47 @@ const filterWorkoutListItemContent = ({
     avgSpeed,
     avgCadence,
     feeling,
+    energyConsumption,
+    multisportSummary,
   };
+};
+
+const getWorkoutMultisportSummaryData = (workoutId: string) => {
+  const workoutRawData = <IWorkoutRawDataContainer>(
+    readJson(getWorkoutRawDataFilename(workoutId))
+  );
+  const multisportExtensionData = getMultisportExtension(
+    workoutRawData.payload
+  );
+
+  if (typeof multisportExtensionData.Markers === "undefined") {
+    return [];
+  }
+
+  const filteredMultisportExtensions = multisportExtensionData.Markers.filter(
+    (activity) => activity.Totals !== null
+  );
+
+  if (typeof filteredMultisportExtensions === "undefined") {
+    return [];
+  }
+
+  return filteredMultisportExtensions.map(
+    ({ ActivityID, Totals: { duration, distance, ascent, hr, speed } }) => {
+      const { avg: avgHr, max: maxHr } = hr !== null ? hr : <ISummary>{};
+      const { avg: avgSpeed, max: maxSpeed } =
+        speed !== null ? speed : <ISummary>{};
+
+      return <IWorkoutMultisportSummaryData>{
+        activityId: ActivityID,
+        duration,
+        distance,
+        ascent,
+        maxHr,
+        avgHr,
+        maxSpeed,
+        avgSpeed,
+      };
+    }
+  );
 };
