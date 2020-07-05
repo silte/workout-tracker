fbf225929c4a0a715fe91605c8fb8cf3f94218e3
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-07-05 20:16:14 +0300

    Add types for workout raw data and getters for extensions

--- backend/src/controllers/workout/getWorkoutData.ts
+++ backend/src/controllers/workout/getWorkoutData.ts
@@ -3,5 +3,6 @@ import { readJson } from "../../utils/jsonHelper";
 
 export const getWorkoutData = (workoutId: string) => {
   const filename = `${DATA_DIR}/${workoutId}.json`;
-  return readJson(filename);
+  const { payload: workoutData } = <IWorkoutRawDataContainer>readJson(filename);
+  return workoutData.extensions.map((extension: any) => extension.type);
 };
