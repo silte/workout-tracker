680b5b691c4567be28e6aaf5eed612464d7e34d4
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-08-05 21:43:28 +0300

    Add mont and week selector buttons to summary page

--- frontend/src/utils/timeConverter.ts
+++ frontend/src/utils/timeConverter.ts
@@ -3,7 +3,9 @@ export const secondsToHms = (totalSeconds: number | undefined = 0) => {
   const minutes = Math.floor((totalSeconds % 3600) / 60);
   const seconds = Math.floor((totalSeconds % 3600) % 60);
 
-  return `${addLeadingZero(hours)}:${addLeadingZero(minutes)}'${addLeadingZero(seconds)} h`;
+  return `${addLeadingZero(hours)}:${addLeadingZero(minutes)}'${addLeadingZero(
+    seconds
+  )} h`;
 };
 
 export const unixtimeToDate = (unixtime: number) => {
@@ -14,14 +16,68 @@ export const unixtimeToDate = (unixtime: number) => {
   const hours = date.getHours();
   const minutes = date.getMinutes();
 
-  return `${day}.${month}.${year} – klo ${addLeadingZero(hours)}.${addLeadingZero(minutes)}`;
+  return `${day}.${month}.${year} – klo ${addLeadingZero(
+    hours
+  )}.${addLeadingZero(minutes)}`;
 };
 
-export const formatDateToISO8601 = (date : Date) => {
+export const formatDateToISO8601 = (date: Date) => {
   const year = date.getUTCFullYear();
   const month = date.getUTCMonth() + 1;
   const day = date.getUTCDate();
-  
+
   return `${year}-${addLeadingZero(month)}-${addLeadingZero(day)}`;
 };
+
+export const getFirstDayOfWeek = (date: Date) => {
+  const dayDiffToMonday = date.getDay() > 0 ? date.getDay() - 1 : 6;
+  const mondayDate = new Date(date);
+
+  mondayDate.setDate(mondayDate.getDate() - dayDiffToMonday);
+  return mondayDate;
+};
+
+export const getLastDayOfWeek = (date: Date) => {
+  const sundayDate = getFirstDayOfWeek(date);
+  sundayDate.setDate(sundayDate.getDate() + 7);
+  return sundayDate;
+};
+
+export const getFirstDayOfNextWeek = (date: Date) => {
+  const nextWeekMonday = getFirstDayOfWeek(date);
+  nextWeekMonday.setDate(nextWeekMonday.getDate() + 7);
+  return nextWeekMonday;
+};
+
+export const getFirstDayOfPreviousWeek = (date: Date) => {
+  const previousWeekMonday = getFirstDayOfWeek(date);
+  previousWeekMonday.setDate(previousWeekMonday.getDate() - 7);
+  return previousWeekMonday;
+};
+
+export const getFirstDayOfMonth = (date: Date) => {
+  const firstDayOfMonth = new Date(date);
+  firstDayOfMonth.setDate(1);
+  return firstDayOfMonth;
+};
+
+export const getLastDayOfMonth = (date: Date) => {
+  const lastDayIfMonth = new Date(date);
+  lastDayIfMonth.setMonth(lastDayIfMonth.getMonth() + 1);
+  lastDayIfMonth.setDate(0);
+  return lastDayIfMonth;
+};
+
+export const getFirstDayOfNextMonth = (date: Date) => {
+  const firstDayOfNextMonth = getFirstDayOfMonth(date);
+  firstDayOfNextMonth.setMonth(firstDayOfNextMonth.getMonth() + 1);
+  return firstDayOfNextMonth;
+};
+
+export const getFirstDayOfPreviousMonth = (date: Date) => {
+  const firstDayOfNextMonth = getFirstDayOfMonth(date);
+  firstDayOfNextMonth.setMonth(firstDayOfNextMonth.getMonth() - 1);
+  return firstDayOfNextMonth;
+};
+
 const addLeadingZero = (time: number) => (time < 10 ? `0${time}` : time);
