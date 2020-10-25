39cca768275794538fa01f587ccf7210050ffc0b
Teemu Sillantaus <teemu.sillantaus@gmail.com>
2020-10-25 20:40:00 +0200

    Add chart data selection data to url

--- frontend/src/containers/workout/WorkoutContainer.tsx
+++ frontend/src/containers/workout/WorkoutContainer.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { WORKOUT_DATA_ENDPOINT } from "../../constants/endpoints";
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import { Workout } from "../../pages/workout/Workout";
 
 export const WorkoutContainer = () => {
   const [workout, setWorkout] = useState<IWorkoutData>({} as IWorkoutData);
-  const { workoutId } = useParams<{ workoutId: string }>();
+  const { workoutId, chartEndIndex: defaultChartEndIndex, chartStartIndex: defaultChartStartIndex } = useParams<{ workoutId: string, chartStartIndex?: string, chartEndIndex?: string }>();
+
+  const [chartStartIndex, setChartStartIndex] = useState<number>(typeof defaultChartStartIndex !== "undefined" ? parseInt(defaultChartStartIndex) : NaN);
+  const [chartEndIndex, setChartEndIndex] = useState<number>(typeof defaultChartEndIndex !== "undefined" ? parseInt(defaultChartEndIndex) : NaN);
+
+  const history = useHistory();
 
   useEffect(() => {
     const fetchWorkout = async () => {
@@ -16,5 +21,13 @@ export const WorkoutContainer = () => {
     fetchWorkout();
   }, [workoutId]);
 
-  return <Workout workout={workout} />;
+  useEffect(() => {
+    let path = `/workout/${workoutId}`;
+    if((!isNaN(chartStartIndex) && !isNaN(chartEndIndex)) && (chartStartIndex > 0 || chartEndIndex < workout.dataPoints?.length - 1)) {
+      path = `/workout/${workoutId}/${chartStartIndex}/${chartEndIndex}`;
+    }
+    history.replace(path);
+  }, [chartStartIndex, chartEndIndex, workoutId, history]);
+
+  return <Workout workout={workout} chartStartIndex={chartStartIndex} chartEndIndex={chartEndIndex} setChartStartIndex={setChartStartIndex} setChartEndIndex={setChartEndIndex} />;
 };
--- frontend/src/pages/workout/Workout.tsx
+++ frontend/src/pages/workout/Workout.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 
 import {
   Line,
@@ -24,16 +24,26 @@ import {
   getRoundedMetres,
 } from "../../utils/distanceConverter";
 import { ButtonGroup } from "@material-ui/core";
+import { useWindowWidth } from '../../containers/workout/windowSize';
 
 interface IWorkout {
   workout: IWorkoutData;
+  chartStartIndex: number;
+  chartEndIndex: number;
+  setChartStartIndex: any
+  setChartEndIndex: any
 }
 
 interface IWorkoutDataPointsChart extends IWorkoutDataPointData {
   timeString?: string;
 }
 
-export const Workout = ({ workout }: IWorkout) => {
+interface BrushStartEndIndex extends IWorkoutDataPointData {
+  startIndex: number;
+  endIndex: number;
+}
+
+export const Workout = ({ workout, chartStartIndex, chartEndIndex, setChartStartIndex, setChartEndIndex }: IWorkout) => {
   const [isHearRateVisible, setIsHearRateVisible] = useState<boolean>(true);
   const [isSpeedVisible, setIsSpeedVisible] = useState<boolean>(true);
   const [isAltitudeVisible, setIsAltitudeVisible] = useState<boolean>(true);
@@ -41,15 +51,14 @@ export const Workout = ({ workout }: IWorkout) => {
 
   const toggleBooleanState = (prevStat: boolean) => !prevStat;
 
-  if (!("activityId" in workout)) {
-    return <h1>Loading...</h1>;
-  }
+
+  const windowWidth = useWindowWidth()
 
   const getWorkoutDurationFromTimestamp = (timestamp: number) => {
     return secondsToHms((timestamp - workout.startTime) / 1000);
   };
 
-  const chartDataPoints: IWorkoutDataPointsChart[] = workout.dataPoints.map(
+  const chartDataPoints: IWorkoutDataPointsChart[] = workout?.dataPoints?.map(
     (dataPoint) => ({
       ...dataPoint,
       timeString: getWorkoutDurationFromTimestamp(
@@ -57,6 +66,30 @@ export const Workout = ({ workout }: IWorkout) => {
       ),
     })
   );
+
+  const handleChartSelectionChange = ({startIndex, endIndex}: BrushStartEndIndex) => {
+    setChartStartIndex(startIndex);
+    setChartEndIndex(endIndex);
+  }
+
+  const memoDataChart = useMemo(() =>         <DataChart 
+  dataPoints={chartDataPoints}
+  isHearRateVisible={isHearRateVisible}
+  isSpeedVisible={isSpeedVisible}
+  isAltitudeVisible={isAltitudeVisible}
+  isCadenceVisible={isCadenceVisible}
+  handleSelectionChange={handleChartSelectionChange}
+  startIndex={chartStartIndex}
+  endIndex={chartEndIndex}
+  />
+, [workout, windowWidth, isHearRateVisible, isSpeedVisible, isAltitudeVisible, isCadenceVisible])
+
+if (!("activityId" in workout)) {
+  return <h1>Loading...</h1>;
+}
+
+  // </>), [workout, windowWidth, isHearRateVisible, isSpeedVisible, isAltitudeVisible, isCadenceVisible]) (
+
 
   return (
     <>
@@ -109,71 +142,86 @@ export const Workout = ({ workout }: IWorkout) => {
         </ButtonGroup>
       </Container>
       <Spacer>
-        <div style={{ width: "100%", height: "33vh", minHeight: "450px" }}>
-          <ResponsiveContainer>
-            <ComposedChart
-              width={1500}
-              height={800}
-              data={chartDataPoints}
-              margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-              maxDataPointsToRender={300}
-            >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="timeString" minTickGap={10} />
-              <YAxis domain={["dataMin", "dataMax"]} />
-              <YAxis
-                domain={["dataMin", "dataMax"]}
-                yAxisId="altitude"
-                hide={true}
-              />
-              <YAxis yAxisId="speed" orientation="right" />
-              <YAxis
-                domain={["dataMin", "dataMax"]}
-                yAxisId="cadence"
-                hide={true}
-              />
-              <Tooltip />
-              <Legend />
-              {isAltitudeVisible && (
-                <Area
-                  type="monotone"
-                  dataKey="altitude"
-                  yAxisId="altitude"
-                  stroke="#dedede"
-                  fill="#dedede"
-                />
-              )}
-              {isHearRateVisible && (
-                <Line
-                  dot={false}
-                  type="monotone"
-                  dataKey="hr"
-                  stroke="#f42424"
-                />
-              )}
-              {isSpeedVisible && (
-                <Line
-                  dot={false}
-                  type="monotone"
-                  dataKey="speed"
-                  yAxisId="speed"
-                  stroke="#8884d8"
-                />
-              )}
-              {isCadenceVisible && (
-                <Line
-                  dot={false}
-                  type="monotone"
-                  dataKey="cadence"
-                  stroke="#82ca9d"
-                  yAxisId="cadence"
-                />
-              )}
-              <Brush dataKey="timeString" />
-            </ComposedChart>
-          </ResponsiveContainer>
-        </div>
+        {memoDataChart}
       </Spacer>
     </>
   );
 };
+
+interface IDataChart {
+  dataPoints: IWorkoutDataPointsChart[];
+  isHearRateVisible: boolean;
+  isSpeedVisible: boolean;
+  isAltitudeVisible: boolean;
+  isCadenceVisible: boolean;
+  handleSelectionChange: any;
+  startIndex: number;
+  endIndex: number;
+}
+
+const DataChart = ({dataPoints, isHearRateVisible, isSpeedVisible, isAltitudeVisible, isCadenceVisible, handleSelectionChange, startIndex, endIndex}: IDataChart) => 
+
+  <div style={{ width: "100%", height: "33vh", minHeight: "450px" }}>
+  <ResponsiveContainer>
+    <ComposedChart
+      width={1500}
+      height={800}
+      data={dataPoints}
+      margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+      maxDataPointsToRender={300}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="timeString" minTickGap={10} />
+      <YAxis domain={["dataMin", "dataMax"]} />
+      <YAxis
+        domain={["dataMin", "dataMax"]}
+        yAxisId="altitude"
+        hide={true}
+      />
+      <YAxis yAxisId="speed" orientation="right" />
+      <YAxis
+        domain={["dataMin", "dataMax"]}
+        yAxisId="cadence"
+        hide={true}
+      />
+      <Tooltip />
+      <Legend />
+      {isAltitudeVisible && (
+        <Area
+          type="monotone"
+          dataKey="altitude"
+          yAxisId="altitude"
+          stroke="#dedede"
+          fill="#dedede"
+        />
+      )}
+      {isHearRateVisible && (
+        <Line
+          dot={false}
+          type="monotone"
+          dataKey="hr"
+          stroke="#f42424"
+        />
+      )}
+      {isSpeedVisible && (
+        <Line
+          dot={false}
+          type="monotone"
+          dataKey="speed"
+          yAxisId="speed"
+          stroke="#8884d8"
+        />
+      )}
+      {isCadenceVisible && (
+        <Line
+          dot={false}
+          type="monotone"
+          dataKey="cadence"
+          stroke="#82ca9d"
+          yAxisId="cadence"
+        />
+      )}
+      <Brush dataKey="timeString" onChange={handleSelectionChange} gap={60} startIndex={startIndex} endIndex={endIndex} />
+    </ComposedChart>
+  </ResponsiveContainer>
+</div>
