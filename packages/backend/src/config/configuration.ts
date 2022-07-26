c2257b10f13a205ee1324f7acd3c7e6a0ada3c41
Teemu Sillantaus <10772816+silte@users.noreply.github.com>
2022-07-26 21:05:30 +0300

    Setup flow to copy api spec to frontend

--- packages/backend/src/config/configuration.ts
+++ packages/backend/src/config/configuration.ts
@@ -4,6 +4,8 @@ const isNotEmptyString = (value: string) => value && value.length > 0;
 
 export const isNodeEnvInTest = () => process.env.NODE_ENV === 'test';
 export const isNodeEnvInDev = () => process.env.NODE_ENV === 'development';
+export const shouldOnlyExportApiSpec = () =>
+  `${process.env.NEST_ONLY_EXPORT_API_SPEC}`.toUpperCase() === 'TRUE';
 
 export const isGithubAuthEnabled = () =>
   !isNodeEnvInTest() &&
